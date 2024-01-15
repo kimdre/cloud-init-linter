6477ffb567583f34d9491f1570704173e2a975ad
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-01-15 22:03:44 +0000

    Fix cloud-init
    Signed-off-by: GitHub <noreply@github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 readarray -t config_files < <(ls cloud-init*.{yml,yaml} -1q 2> /dev/null)
 had_errors=0
 for config_file in "${config_files[@]}"; do
-    result="$(cloud-init devel schema --config-file $config_file 2>&1)"
+    result="$(cloud-init schema --config-file $config_file 2>&1)"
     if [[ "$?" -eq 0 ]]
     then
         echo "$config_file was valid"
